1e8d507dafe62f2565ab5d9252cf3a21db5fe502
Manuel Koch <mko0815@gmail.com>
2015-11-10 23:48:19 +0100

    Remove ImageFactoryPreview.

--- src/imagefactory.h
+++ src/imagefactory.h
@@ -2,7 +2,6 @@
 #define IMAGEFACTORY_H
 
 #include "imagefactoryworker.h"
-#include "imagefactorypreview.h"
 #include "imagefactoryunderexposed.h"
 #include "imagefactoryoverexposed.h"
 #include "histfactory.h"
@@ -13,7 +12,6 @@ class ImageFactory : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(QUrl         image        READ image        NOTIFY imageChanged)
-    Q_PROPERTY(QUrl         preview      READ preview      NOTIFY previewChanged)
     Q_PROPERTY(QUrl         underExposed READ underExposed NOTIFY underExposedChanged)
     Q_PROPERTY(QUrl         overExposed  READ overExposed  NOTIFY overExposedChanged)
     Q_PROPERTY(HistFactory* histogram    READ histogram    CONSTANT)
@@ -23,7 +21,6 @@ public:
     ~ImageFactory();
 
     QUrl image()        const { return m_image.url(); }
-    QUrl preview()      const { return m_preview.url(); }
     QUrl underExposed() const { return m_underExposed.url(); }
     QUrl overExposed()  const { return m_overExposed.url(); }
     HistFactory *histogram() { return &m_histFactory; }
@@ -31,14 +28,12 @@ public:
 signals:
 
     void imageChanged(QUrl url);
-    void previewChanged(QUrl url);
     void underExposedChanged(QUrl url);
     void overExposedChanged(QUrl url);
 
 private:
 
     ImageFactoryWorker        m_image;
-    ImageFactoryPreview       m_preview;
     ImageFactoryUnderExposed  m_underExposed;
     ImageFactoryOverExposed   m_overExposed;
     HistFactory               m_histFactory;
